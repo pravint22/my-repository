6a293fc4d8adc113a6a0b4fe9478ea61afaa8fb0
pravin <pravintanpure1233@gmail.com>
2025-02-15 14:42:50 +0530

    add requirements to json

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 // import Button from './Button.jsx'
+// Change from BrowserRouter to HashRouter in your App.jsx
+// import { HashRouter as Router } from 'react-router-dom'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
